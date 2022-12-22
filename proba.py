d93917ac59e43e8c746ad4963405eac7eaac676b
sigzer <82393336+sigzer@users.noreply.github.com>
2022-12-22 12:31:26 +0100

    Add files via upload

--- proba.py
+++ proba.py
@@ -27,7 +27,7 @@ else:
     print(f"The number {number} is exactly zero")
 
 #Write a program to display the calendar of a given date.
-
+#i made a change here
 
 yy = int(input("Enter today's year: "))
 mm = int(input("Enter today's month: "))
@@ -64,6 +64,61 @@ string_two = str(input("Enter the second string: "))
 string_final = string_one + " " + string_two
 print(string_final)
 
+#Write a program to check if an item exists in the list
+
+myList = ['narancs', 'alma', 'korte', 98, 43.9843, True, 5843]
+
+s = 'narancs', 'alma', 'korte'
+matched_indexes = []
+i = 0
+length = len(myList)
+
+while i < length:
+    if s == myList[i]:
+        matched_indexes.append(i)
+    i += 1
+
+print(f"matched indexes are: {matched_indexes} in the list: {myList}")
+
+#Write a program to join two or more lists
+
+list_one = ['This', 'is', 'list', 1]
+list_two = ['This', 'is', 'not', 'list', 1, 'but', 'list', 2]
+list_three = [3, 333, 33, 3.33]
+
+#this is the part where I join more strings together
+all_lists = list_one + list_two + list_three
+word = 'is'
+#at this statement I wanted to remove all the 'is' strings 
+#i had to do it twice, because .remove(*) only removes the first element 
+if all_lists.count(word) > 1:
+    print(all_lists.remove(word), all_lists.remove(word))
+    print(all_lists)
+else:
+    print("shitas")
+
+
+#Write a program to calculate cube of a number.
+
+x = float(input("Enter a number: ")) # I did it with float numbers so we can calculate the cube of int and float numbers
+y = x ** 2 
+
+print(y)
+
+#Write a program to calculate square root of a number
+
+x = float(input("Enter the number which you want to know the root: "))
+y = x ** (1 / 2)
+
+print(y)
+
+#Write a program that takes a list of numbers (for example, i = [6, 10, 75, 60, 55]) and
+#makes a new list of only the first and last elements of the given list.
+
+i = [6, 10, 75, 60, 55]
+ #   0   1   2   3   4 
+
+print(i[0])
 
 
 
